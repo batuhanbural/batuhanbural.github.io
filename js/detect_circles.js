6ffd77b9bb57b1239e66abc03e6a573262576b0e
Batuhan BURAL <60934501+batuhanbural@users.noreply.github.com>
2021-06-24 00:07:43 +0300

    Update detect_circles.js

--- js/detect_circles.js
+++ js/detect_circles.js
@@ -19,7 +19,6 @@ function onImageSelect() {
     cv.imshow('imageCanvas', image);
     image.delete();
     document.getElementById("imageCanvas").classList.remove("hidden")
-    document.getElementById("circlesButton").classList.remove("disabled")
 }
 
 // create canvas for js drawings
@@ -31,53 +30,157 @@ document.getElementById("img-back").onclick = function () {
     onImageSelect()
 }
 
-// detecting pupil
-document.getElementById('pupilButton').onclick = function () {
-    this.disabled = true;
-    document.body.classList.add('loading');
+// resize image
+document.getElementById("resize_img").onclick = function () {
+    let src = cv.imread('imageCanvas');
+    let dst = new cv.Mat();
+    let dsize = new cv.Size(2560, 1440);
+    // You can try more different parameters
+    cv.resize(src, dst, dsize, 0, 0, cv.INTER_AREA);
+    cv.imshow('imageCanvas', dst);
+    src.delete();
+    dst.delete();
+}
 
-    let srcMat = cv.imread('imageCanvas');
-    let displayMat = srcMat.clone();
-    let circlesMat = new cv.Mat();
+// gray scale
+document.getElementById("gray_scale").onclick = function () {
+    let src = cv.imread('imageCanvas');
+    let dst = new cv.Mat();
+    cv.cvtColor(src, dst, cv.COLOR_RGBA2GRAY);
+    cv.imshow('imageCanvas', dst);
+    src.delete();
+    dst.delete();
 
-    cv.cvtColor(srcMat, srcMat, cv.COLOR_RGBA2GRAY);
-
-    cv.HoughCircles(srcMat, circlesMat, cv.HOUGH_GRADIENT, 1, 45, 75, 40, 0, 0);
-    // cv.HoughCircles(srcMat, circlesMat, cv.HOUGH_GRADIENT, 1, 100, 30, 50, 19, 50);
-
-    for (let i = 0; i < circlesMat.cols; ++i) {
-        let x = circlesMat.data32F[i * 3];
-        let y = circlesMat.data32F[i * 3 + 1];
-        let radius = circlesMat.data32F[i * 3 + 2];
-        let center = new cv.Point(x, y);
-
-        // draw circles
-        console.log(center)
-        cv.circle(displayMat, center, radius, [116, 15, 245, 96], 2);
-    }
-
-    cv.imshow('imageCanvas', displayMat);
-
-    // delete unnecessary items
-    srcMat.delete();
-    displayMat.delete();
-    circlesMat.delete();
-
-    this.disabled = false;
-    document.body.classList.remove('loading');
     document.getElementById("img-back").style.removeProperty("display");
     document.getElementById("downloadButton").classList.remove('disabled');
-};
+}
 
-// detecting light
-document.getElementById('lightButton').onclick = function () {
-    this.disabled = true;
-    document.body.classList.add('loading');
+// median blurring image
+document.getElementById("median_blur").onclick = function () {
+    let src = cv.imread('imageCanvas');
+    let dst = new cv.Mat();
+    // You can try more different parameters
+    cv.medianBlur(src, dst, 25);
+    cv.imshow('imageCanvas', dst);
+    src.delete();
+    dst.delete();
+}
 
+// thresh binary inv (must use with gray-scale image)
+document.getElementById("thresh_binary_inv").onclick = function () {
+    let src = cv.imread('imageCanvas');
+    let dst = new cv.Mat();
+    cv.cvtColor(src, src, cv.COLOR_RGBA2GRAY);
+
+    cv.threshold(src, dst, 177, 255, cv.THRESH_BINARY_INV); // here
+    cv.imshow('imageCanvas', dst);
+    src.delete();
+    dst.delete();
+
+    document.getElementById("img-back").style.removeProperty("display");
+    document.getElementById("downloadButton").classList.remove('disabled');
+}
+
+// thresh binary
+document.getElementById("thresh_binary").onclick = function () {
+    let src = cv.imread('imageCanvas');
+    let dst = new cv.Mat();
+    cv.threshold(src, dst, 200, 255, cv.THRESH_BINARY);
+    cv.imshow('imageCanvas', dst);
+    src.delete();
+    dst.delete();
+
+    document.getElementById("img-back").style.removeProperty("display");
+    document.getElementById("downloadButton").classList.remove('disabled');
+}
+
+// thresh binary hard
+document.getElementById("thresh_binary_hard").onclick = function () {
+    let src = cv.imread('imageCanvas');
+    let dst = new cv.Mat();
+    // You can try more different parameters
+    cv.threshold(src, dst, 253, 255, cv.THRESH_BINARY);
+    cv.imshow('imageCanvas', dst);
+    src.delete();
+    dst.delete();
+
+    document.getElementById("img-back").style.removeProperty("display");
+    document.getElementById("downloadButton").classList.remove('disabled');
+}
+
+// hough circles
+document.getElementById("hough_circles").onclick = function () {
+    let src = cv.imread('imageCanvas');
+    let circles = new cv.Mat();
+    cv.cvtColor(src, src, cv.COLOR_RGBA2GRAY, 0);
+    // You can try more different parameters
+    cv.HoughCircles(src, circles, cv.HOUGH_GRADIENT,
+        1, 100, 30, 50, 75, 270);
+
+    // draw circles
+    for (let i = 0; i < circles.cols; ++i) {
+        let x = circles.data32F[i * 3];
+        let y = circles.data32F[i * 3 + 1];
+        let radius = circles.data32F[i * 3 + 2];
+        let center = new cv.Point(x, y);
+        console.log(center, radius)
+        cv.circle(src, center, radius, [255, 0, 0, 46], 4);
+    }
+    cv.imshow('imageCanvas', src);
+    src.delete();
+    circles.delete();
+}
+
+// find pupils
+document.getElementById("find_pupil").onclick = function () {
+
+}
+
+// gaussian blur
+document.getElementById("gaussian_blur").onclick = function () {
+    let src = cv.imread('imageCanvas');
+    let dst = new cv.Mat();
+    let ksize = new cv.Size(11, 11);
+    // You can try more different parameters
+    cv.GaussianBlur(src, dst, ksize, 0, 0, cv.BORDER_DEFAULT);
+    cv.imshow('imageCanvas', dst);
+    src.delete();
+    dst.delete();
+}
+
+// erode
+document.getElementById("erode").onclick = function () {
+    let src = cv.imread('imageCanvas');
+    let dst = new cv.Mat();
+    let M = cv.Mat.ones(5, 5, cv.CV_8U);
+    let anchor = new cv.Point(-1, -1);
+    // You can try more different parameters
+    cv.erode(src, dst, M, anchor, 2, cv.BORDER_CONSTANT, cv.morphologyDefaultBorderValue());
+    cv.imshow('imageCanvas', dst);
+    src.delete();
+    dst.delete();
+    M.delete();
+}
+
+// dilate
+document.getElementById("dilate").onclick = function () {
+    let src = cv.imread('imageCanvas');
+    let dst = new cv.Mat();
+    let M = cv.Mat.ones(5, 5, cv.CV_8U);
+    let anchor = new cv.Point(-1, -1);
+    // You can try more different parameters
+    cv.dilate(src, dst, M, anchor, 4, cv.BORDER_CONSTANT, cv.morphologyDefaultBorderValue());
+    cv.imshow('imageCanvas', dst);
+    src.delete();
+    dst.delete();
+    M.delete();
+}
+
+// find light
+document.getElementById("find_light").onclick = function () {
     let src = cv.imread('imageCanvas');
     let dst = cv.Mat.zeros(src.rows, src.cols, cv.CV_8UC3);
     cv.cvtColor(src, src, cv.COLOR_RGBA2GRAY, 0);
-    cv.threshold(src, src, 190, 255, cv.THRESH_BINARY);
     let contours = new cv.MatVector();
     let hierarchy = new cv.Mat();
     cv.findContours(src, contours, hierarchy, cv.RETR_CCOMP, cv.CHAIN_APPROX_SIMPLE);
@@ -100,119 +203,6 @@ document.getElementById('lightButton').onclick = function () {
     contours.delete();
     hierarchy.delete();
     cnt.delete();
-
-
-    this.disabled = false;
-    document.body.classList.remove('loading');
-    document.getElementById("img-back").style.removeProperty("display");
-    document.getElementById("downloadButton").classList.remove('disabled');
-};
-
-// Detecting Circles
-document.getElementById("circleDetect").onclick = function () {
-    let src = cv.imread('imageCanvas');
-    let dst = cv.Mat.zeros(src.rows, src.cols, cv.CV_8U);
-    let circles = new cv.Mat();
-    let color = new cv.Scalar(255, 255, 0);
-    cv.cvtColor(src, src, cv.COLOR_RGBA2GRAY, 0);
-// You can try more different parameters
-    cv.HoughCircles(src, circles, cv.HOUGH_GRADIENT,
-        1, 45, 75, 40, 50, 80);
-// draw circles
-    for (let i = 0; i < circles.cols; ++i) {
-        let x = circles.data32F[i * 3];
-        let y = circles.data32F[i * 3 + 1];
-        let radius = circles.data32F[i * 3 + 2];
-        let center = new cv.Point(x, y);
-        cv.circle(src, center, radius, color);
-    }
-    cv.imshow('imageCanvas', src);
-    src.delete();
-    dst.delete();
-    circles.delete();
-
-    document.getElementById("img-back").style.removeProperty("display");
-
-}
-
-// convex hull canvases
-document.getElementById("convexHull").onclick = function () {
-    let src = cv.imread('imageCanvas');
-    let dst = cv.Mat.zeros(src.rows, src.cols, cv.CV_8UC3);
-    cv.cvtColor(src, src, cv.COLOR_RGBA2GRAY, 0);
-    cv.threshold(src, src, 100, 200, cv.THRESH_BINARY);
-    let contours = new cv.MatVector();
-    let hierarchy = new cv.Mat();
-    let hull = new cv.MatVector();
-    cv.findContours(src, contours, hierarchy, cv.RETR_CCOMP, cv.CHAIN_APPROX_SIMPLE);
-    for (let i = 0; i < contours.size(); ++i) {
-        let tmp = new cv.Mat();
-        let cnt = contours.get(i);
-        // You can try more different parameters
-        cv.convexHull(cnt, tmp, false, true);
-        hull.push_back(tmp);
-        cnt.delete();
-        tmp.delete();
-    }
-// draw contours with random Scalar
-    for (let i = 0; i < contours.size(); ++i) {
-        let colorHull = new cv.Scalar(Math.round(Math.random() * 255), Math.round(Math.random() * 255),
-            Math.round(Math.random() * 255));
-        cv.drawContours(dst, hull, i, colorHull, 1, 8, hierarchy, 0);
-    }
-    cv.imshow('imageCanvas', dst);
-    src.delete();
-    dst.delete();
-    hierarchy.delete();
-    contours.delete();
-    hull.delete();
-
-}
-
-// approx polly canvases
-document.getElementById("approxPollyDP").onclick = function () {
-    let src = cv.imread('imageCanvas');
-    let dst = cv.Mat.zeros(src.rows, src.cols, cv.CV_8UC3);
-    cv.cvtColor(src, src, cv.COLOR_RGBA2GRAY, 0);
-    cv.threshold(src, src, 100, 200, cv.THRESH_BINARY);
-    let contours = new cv.MatVector();
-    let hierarchy = new cv.Mat();
-    let poly = new cv.MatVector();
-    cv.findContours(src, contours, hierarchy, cv.RETR_CCOMP, cv.CHAIN_APPROX_SIMPLE);
-// approximates each contour to polygon
-    for (let i = 0; i < contours.size(); ++i) {
-        let tmp = new cv.Mat();
-        let cnt = contours.get(i);
-        // You can try more different parameters
-        cv.approxPolyDP(cnt, tmp, 3, true);
-        poly.push_back(tmp);
-        cnt.delete();
-        tmp.delete();
-    }
-// draw contours with random Scalar
-    for (let i = 0; i < contours.size(); ++i) {
-        let color = new cv.Scalar(Math.round(Math.random() * 255), Math.round(Math.random() * 255),
-            Math.round(Math.random() * 255));
-        cv.drawContours(dst, poly, i, color, 1, 8, hierarchy, 0);
-    }
-    cv.imshow('imageCanvas', dst);
-    src.delete();
-    dst.delete();
-    hierarchy.delete();
-    contours.delete();
-    poly.delete();
-}
-
-// threshold
-document.getElementById("threshold").onclick = function () {
-    let src = cv.imread('imageCanvas');
-    let dst = new cv.Mat();
-// You can try more different parameters
-    cv.threshold(src, dst, 250, 255, cv.THRESH_BINARY);
-    cv.imshow('imageCanvas', dst);
-    src.delete();
-    dst.delete();
-
 }
 
 
@@ -221,4 +211,3 @@ document.getElementById('downloadButton').onclick = function () {
     this.href = document.getElementById('imageCanvas').toDataURL();
     this.download = 'image.png';
 };
-
